6da0709c6f4530d9d02f61979aae93df5442e976
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-11-19 19:20:54 +0000

    Deploying to gh-pages from @ DmytroNadolynskyi/goit-js-hw-09@0f29cf3e37fb8d155f03a39415e4c0b4769911e3 🚀

---
+++ 01-color-switcher.6922e5ca.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.6922e5ca.js.map
---
+++ 01-color-switcher.b64d6e3d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d;e.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.b64d6e3d.js.map
